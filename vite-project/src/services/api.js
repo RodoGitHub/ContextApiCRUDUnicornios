47356556ea1524e3bb65342ee730dd57ba0f024f
RodoGitHub <r.palacios@itecriocuarto.org.ar>
2025-04-29 20:57:44 -0300

    ordenando codigo y nopmbre de variables

--- vite-project/src/services/api.js
+++ vite-project/src/services/api.js
@@ -1,7 +1,7 @@
-// const BASE_URL = 'https://crudcrud.com/api/487c5f23af1f48a98559414db9647424/unicorns';
+// const URL_API = 'https://crudcrud.com/api/487c5f23af1f48a98559414db9647424/unicorns';
 
 // export const createObject = async (data) => {
-//   const response = await fetch(BASE_URL, {
+//   const response = await fetch(URL_API, {
 //     method: 'POST',
 //     headers: { 'Content-Type': 'application/json' },
 //     body: JSON.stringify(data),
@@ -12,7 +12,7 @@
 // export const updateObject = async (id, data) => {
 //   console.log("update",id);
 //   console.log("data",data);
-//   const response = await fetch(`${BASE_URL}/${id}`, {
+//   const response = await fetch(`${URL_API}/${id}`, {
 //     method: 'PUT',
 //     headers: { 'Content-Type': 'application/json' },
 //     body: JSON.stringify(data),
@@ -22,14 +22,14 @@
 
 // export const deleteObject = async (id) => {
 //   console.log("borrar",id);
-//   const response = await fetch(`${BASE_URL}/${id}`, {
+//   const response = await fetch(`${URL_API}/${id}`, {
 //     method: 'DELETE',
 //   });
 //   return response.ok;
 // };
 
 // export const getObjects = async () => {
-//   const response = await fetch(`${BASE_URL}`);
+//   const response = await fetch(`${URL_API}`);
 //   if (!response.ok) {
 //     throw new Error('Error fetching data');
 //   }
@@ -38,11 +38,11 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'https://crudcrud.com/api/1d287b78ddb443d7b92912b6df04c5fc/unicorns';
+const URL_API = 'https://crudcrud.com/api/1d287b78ddb443d7b92912b6df04c5fc/unicorns';
 
 export const createObject = async (data) => {
   try {
-    const response = await axios.post(BASE_URL, data);
+    const response = await axios.post(URL_API, data);
     return response.status === 201; 
   } catch (error) {
     console.error("Error creando unicornio:", error);
@@ -52,7 +52,7 @@ export const createObject = async (data) => {
 
 export const updateObject = async (id, data) => {
   try {
-    const response = await axios.put(`${BASE_URL}/${id}`, data);
+    const response = await axios.put(`${URL_API}/${id}`, data);
     return response.status === 200; 
   } catch (error) {
     console.error("Error actualizando unicornio:", error);
@@ -63,7 +63,7 @@ export const updateObject = async (id, data) => {
 export const deleteObject = async (id) => {
   console.log("borrar", id);
   try {
-    const response = await axios.delete(`${BASE_URL}/${id}`);
+    const response = await axios.delete(`${URL_API}/${id}`);
     return response.status === 200; 
   } catch (error) {
     console.error("Error borrando unicornio:", error);
@@ -73,10 +73,10 @@ export const deleteObject = async (id) => {
 
 export const getObjects = async () => {
   try {
-    const response = await axios.get(BASE_URL);
+    const response = await axios.get(URL_API);
     return response.data;
   } catch (error) {
-    console.error("Error fetch data:", error);
+    console.error("Error get data:", error);
   
   }
 };
